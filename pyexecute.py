daf89deabeee1849cfb2ad1e75c72ac7a1ed6fbd
Conaxian <accounts2@jeniweb.eu>
2021-08-07 20:33:28 +0200

    Rewrite everything

--- pyexecute.py
+++ pyexecute.py
@@ -1,64 +1,67 @@
 import os
 import time
 import ast
+import traceback
 import subprocess
 import signal
+from typing import Union
 
-allowed_modules = ["datetime", "math", "random", "hashlib", "time", "getpass", "socket", "urllib"]
-banned_names = ["exec", "eval", "compile", 
-"globals", "locals", "vars", "builtins", "dir", 
-"open", "input", "breakpoint", "getattr", "delattr",
-"__dict__", "__base__"]
+ALLOWED_MODULES = [
+    "datetime",
+    "math",
+    "random",
+    "hashlib",
+    "time",
+    "getpass",
+    "socket",
+    "urllib"
+]
+BANNED_NAMES = [
+    "exec",
+    "eval",
+    "compile",
+    "globals",
+    "locals",
+    "vars",
+    "builtins",
+    "dir",
+    "open",
+    "input",
+    "breakpoint",
+    "getattr",
+    "delattr",
+    "__dict__",
+    "__base__"
+]
 
 class ExecTimeoutError(Exception):
-    pass
+    """
+    Raised when the execution times out.
+    """
 
 class UnsafeCodeError(Exception):
-    pass
+    """
+    Raised when the passed code is unsafe.
+    """
 
 class Result:
+    """
+    Represents an execution result.
+    """
 
-    def __init__(self, stdout, stderr, exec_time):
-
+    def __init__(self, stdout: str, stderr: str, exec_time: int):
         self.stdout = stdout
         self.stderr = stderr
         self.exec_time = exec_time
 
-class PyExecute:
+class PyExecutor:
 
-    class _Task:
-
-        def __init__(self, executor, code):
-
-            self.executor = executor
-            self.code = code
-
-        def is_running(self):
-
-            if self.executor.win:
-                tasklist_cmd = f'tasklist /FI "pid eq {self.pid}"'
-                tasklist_cond = "INFO: No tasks are running"
-            else:
-                tasklist_cmd = f"ps -p {self.pid}"
-                tasklist_cond = "<defunct>"
-            tasklist = os.popen(tasklist_cmd).read()
-            return tasklist_cond not in tasklist
-
-        def run(self):
-
-            self.exec_start = time.time()
-            with open(self.executor.file_path, "w", encoding="utf-8") as file:
-                file.write(self.code)
-
-            popen_args = [self.executor.python_cmd, self.executor.file_path]
-            popen_kwargs = {"stdout": subprocess.PIPE, "stderr": subprocess.PIPE}
-            if self.executor.win:
-                popen_kwargs["creationflags"] = subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_NO_WINDOW
-            self.process = subprocess.Popen(popen_args, **popen_kwargs)
-            self.pid = self.process.pid
-
-    def __init__(self, filename, timeout=5, checks_per_second=40, python_cmd=None):
-
+    def __init__(self,
+        filename: str,
+        timeout: Union[int, float]=5,
+        checks_per_second: int=40,
+        python_cmd: str=None,
+    ):
         self.filename = filename
         self.timeout = timeout
         self.check_interval = 1 / checks_per_second
@@ -67,40 +70,119 @@ class PyExecute:
         current_path = os.path.abspath(current_dir)
         self.file_path = os.path.join(current_path, filename)
         self.win = os.name == "nt"
-        self.python_cmd = python_cmd or ("python" if self.win else "python3")
+        self.python_cmd = python_cmd or \
+            ("python" if self.win else "python3")
 
-    def scan(self, code):
-
-        tree = ast.parse(code)
-        for node in ast.walk(tree):
-            print(node)
-            if isinstance(node, ast.Name) and node.id in banned_names:
-                raise UnsafeCodeError(node.id)
-            if isinstance(node, ast.Attribute) and node.attr in banned_names:
-                raise UnsafeCodeError(node.attr)
-            if isinstance(node, ast.Import):
-                for alias in node.names:
-                    if alias.name not in allowed_modules:
-                        raise UnsafeCodeError(alias.name)
-            if isinstance(node, ast.ImportFrom) and node.module not in allowed_modules:
-                raise UnsafeCodeError(node.module)
-
-    def execute(self, code):
-
+    def execute(self, code: str, scan: bool=True):
+        """
+        Safely executes Python code.
+        """
         code = code.strip(" \t\n")
-        task = self._Task(self, code)
-        self.scan(code)
-        task.run()
+        task = _Task(self, code)
 
+        if not scan:
+            try:
+                self.scan(code)
+            except SyntaxError:
+                error = traceback.format_exc(limit=0)
+                return Result("", error, 0)
+
+        task.run()
         limit = time.time() + self.timeout
+
         while time.time() < limit:
             time.sleep(self.check_interval)
             if not task.is_running():
                 result = task.process.communicate()
-                stdout, stderr = [bytestr.decode("utf-8").strip() for bytestr in result]
+                stdout, stderr = (bytestr.decode("utf8").strip() \
+                    for bytestr in result)
                 exec_time = time.time() - task.exec_start
                 return Result(stdout, stderr, exec_time)
 
-        kill_signal = signal.CTRL_C_EVENT if self.win else signal.SIGTERM
-        os.kill(task.pid, kill_signal)
+        task.kill()
         raise ExecTimeoutError(time.time() - task.exec_start)
+
+    @staticmethod
+    def scan(code: str):
+        """
+        Scans the passed code and determines
+        whether it's safe or not.
+        """
+        tree = ast.parse(code)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Name) and \
+                node.id in BANNED_NAMES:
+                raise UnsafeCodeError(node.id)
+
+            if isinstance(node, ast.Attribute) and \
+                node.attr in BANNED_NAMES:
+                raise UnsafeCodeError(node.attr)
+
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    if alias.name not in ALLOWED_MODULES:
+                        raise UnsafeCodeError(alias.name)
+
+            if isinstance(node, ast.ImportFrom) and \
+                node.module not in ALLOWED_MODULES:
+                raise UnsafeCodeError(node.module)
+
+class _Task:
+    """
+    Represents a single code execution task.
+    This class should only be instantiated using an executor.
+    """
+
+    def __init__(self, executor: PyExecutor, code: str):
+        self.executor = executor
+        self.code = code
+
+    def is_running(self) -> bool:
+        """
+        Checks if the task is still running.
+        """
+        if self.executor.win:
+            tasklist_cmd = f'tasklist /FI "pid eq {self.pid}"'
+            tasklist_cond = "INFO: No tasks are running"
+        else:
+            tasklist_cmd = f"ps -p {self.pid}"
+            tasklist_cond = "<defunct>"
+        tasklist = os.popen(tasklist_cmd).read()
+        return tasklist_cond not in tasklist
+
+    def run(self):
+        """
+        Runs the task.
+        """
+        self.exec_start = time.time()
+        with open(self.executor.file_path, "w",
+            encoding="utf8") as file:
+            file.write(self.code)
+
+        popen_args = [
+            self.executor.python_cmd,
+            self.executor.file_path,
+        ]
+        popen_kwargs = {
+            "stdout": subprocess.PIPE,
+            "stderr": subprocess.PIPE,
+        }
+        if self.executor.win:
+            popen_kwargs["creationflags"] = \
+                subprocess.CREATE_NEW_PROCESS_GROUP | \
+                subprocess.CREATE_NO_WINDOW
+
+        self.process = subprocess.Popen(popen_args, **popen_kwargs)
+        self.pid = self.process.pid
+
+    def kill(self):
+        """
+        Kills the task.
+        """
+        kill_signal = signal.CTRL_BREAK_EVENT \
+            if self.executor.win else signal.SIGKILL
+        os.kill(self.pid, kill_signal)
+
+executor = PyExecutor("test.py")
+result = executor.execute("while True: print('kekus')")
+print(result.stdout)
